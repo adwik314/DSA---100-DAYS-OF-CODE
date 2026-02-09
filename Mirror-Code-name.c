27b5c029a57c491089060d91f9b19d8aed14cd60
ADWIK SINGH <adwik314@gmail.com>
2026-02-09 09:17:19 +0530

    Create Mirror-Code-name.c

---
+++ Mirror-Code-name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char s[1000];
+    int i, len = 0;
+    printf("Enter the String: ");
+    scanf("%s", s);
+
+    while (s[len] != '\0') {
+        len++;
+    }
+
+    for (i = len - 1; i >= 0; i--) {
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
