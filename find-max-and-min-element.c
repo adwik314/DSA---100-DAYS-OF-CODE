11a05bd8203b601b56a773d7a875c00512bdc9e3
ADWIK SINGH <adwik314@gmail.com>
2026-02-17 08:47:28 +0530

    Create find-max-and-min-element.c

---
+++ find-max-and-min-element.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int max = a[0], min = a[0];
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max)
+            max = a[i];
+        if (a[i] < min)
+            min = a[i];
+    }
+
+    printf("Max: %d\n", max);
+    printf("Min: %d", min);
+
+    return 0;
+}
